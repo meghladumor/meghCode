b7394cca681efb90f9a27fae771194a8289fd9d8
meghladumor <meghladumor9@gmail.com>
2024-03-02 16:42:55 -0500

    New code

---
+++ src/com/meghcode/Scan.java
@@ -0,0 +1,11 @@
+package com.meghcode;
+
+import java.sql.SQLOutput;
+
+public class Scanner {
+    public static void main(String[] args) {
+        System.out.println(" Hello! What is your name :)");
+        Scanner Scanner
+
+    }
+}
